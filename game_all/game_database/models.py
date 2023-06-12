dde7a7d62ee303333aa9e7b236485296380957be
loright <a.jI@bk.ru>
2023-06-13 01:40:18 +0300

    Added prototypes of models

--- game_all/game_database/models.py
+++ game_all/game_database/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+
+# class Player
+# id: int, primary key
+# main_alias: str
+# other_aliases: str
+# telegram_login: str
+# phone: str
+# banks: str
+
+
+# class Game
+# id: int, primary key
+# url: str
+# ledger: str
+# picture: byte
+
+
+# class Payment
+# id: int, primary key
+# game: Game
+# payer: Player
+# recipient: Player
+# value: float
+# paid: bool
+# received: bool
+
+
+# class Team
+# id: int, primary key
+# players: list[Player]
+# admin: Player
+
+
+
