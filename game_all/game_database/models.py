266ef6f70f67fe4aa8e2b036149b853241255348
loright <a.jI@bk.ru>
2023-06-13 03:36:45 +0300

    Added functions

--- game_all/game_database/models.py
+++ game_all/game_database/models.py
@@ -9,13 +9,26 @@ from django.db import models
 # telegram_login: str
 # phone: str
 # banks: str
+# функции: 
+	# add_alias: добавляет alias в строку other_aliases через запятую
+	# change_alias: изменяет main_alias на новый, если новый был в строке other_aliases, то его оттуда удалить, текущий можно записать в other_aliases (не точно)
+	# add_phone: добавить phone
+	# change_phone: изменить phone
+	# add_bank: добавить bank
+	# change_bank: изменить bank
+	# change_telegram: изменить telegram
+
 
 
 # class Game
 # id: int, primary key
 # url: str
 # ledger: str
-# picture: byte
+# picture: image
+# функции: 
+	# create_game: создаёт новую игру по ссылке
+
+
 
 
 # class Payment
@@ -26,12 +39,16 @@ from django.db import models
 # value: float
 # paid: bool
 # received: bool
+# функции: 
+	# add_paymment: добавляет платеж в систему
+	# set_payment_paied: выставляет paid=True
+	# set_payment_received: выставляет received=True
 
 
 # class Team
 # id: int, primary key
 # players: list[Player]
 # admin: Player
-
-
-
+# функции: 
+	# create_team: создаёт новую команду
+	# add_member: добавляет участника в команду
